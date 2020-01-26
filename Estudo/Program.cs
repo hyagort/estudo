0882d9739086b94003430a6227b87b3edd6c3444
Hyago Ruiz-Tagle <hyagocrt@gmail.com>
2020-01-26 14:25:01 -0300

    Aula 115, enums

---
+++ Estudo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Estudo.Entities;
+using Estudo.Entities.Enums;
+using System;
+
+namespace Estudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+
+            Console.WriteLine(order);
+        }
+    }
+}
