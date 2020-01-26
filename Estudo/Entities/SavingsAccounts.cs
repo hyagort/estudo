ebff7559275bc9f2169dd9f1d9943627edee66fb
Hyago Ruiz-Tagle <hyagocrt@gmail.com>
2020-01-26 16:59:57 -0300

    Aula 126 - Up/Downcast

---
+++ Estudo/Entities/SavingsAccounts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudo.Entities
+{
+    class SavingsAccounts : Account
+    {
+
+        public double InterestRate { get; set; }
+
+        public SavingsAccounts()
+        {
+        }
+
+        public SavingsAccounts(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- Estudo/Program.cs
+++ Estudo/Program.cs
@@ -8,7 +8,31 @@ namespace Estudo
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Account account = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //Upcast
+
+            Account acc1 = bacc;
+            Account acc2 = new SavingsAccounts(1004, "Anna", 0.0, 0.01);
+
+            //Downcast
+
+            BusinessAccount acc3 = (BusinessAccount)acc1;
+            acc3.Loan(100.0);
+
+            if (acc2 is BusinessAccount)
+            {
+                BusinessAccount acc4 = (BusinessAccount)acc2;
+            }
+            else if (acc2 is SavingsAccounts)
+            {
+                SavingsAccounts acc4 = (SavingsAccounts)acc2;
+                acc4.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+
 
             Console.WriteLine(account.Balance);
 
